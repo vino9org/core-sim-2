ad5b51448d28ec3f50aa959c72544f75ddbaeb29
Li Lin <guru.lin@gmail.com>
2024-06-23 15:47:00 +0800

    add default database uri, otherwise module cannot be loaded

--- database.py
+++ database.py
@@ -7,6 +7,6 @@ __all__ = ["SessionLocal", "engine"]
 
 load_dotenv()
 
-db_url = os.environ.get("SQLALCHEMY_DATABASE_URI", "")
+db_url = os.environ.get("SQLALCHEMY_DATABASE_URI", "sqlite+aiosqlite:///memory:")
 engine = create_async_engine(db_url, echo=False)
 SessionLocal = async_sessionmaker(bind=engine, autoflush=False, future=True)
